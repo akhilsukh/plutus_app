b1f54860f8c9f4b653ce0da740c3df0a9ba0eb24
Akhil Sukhthankar <akhilsukh01@gmail.com>
2023-11-17 13:37:31 -0800

    Initial Commit

---
+++ plutus/plutusApp.swift
@@ -0,0 +1,32 @@
+//
+//  plutusApp.swift
+//  plutus
+//
+//  Created by Akhil Sukhthankar on 11/17/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct plutusApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
